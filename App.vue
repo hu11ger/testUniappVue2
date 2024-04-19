7a6453e7d0f9ba7dc4df6348ebf1c9d88405e34f
hu11ger <939224682@qq.com>
2024-04-19 18:55:05 +0800

    修复引入css问题；
    引入uview会导致少部分uni-ui出现样式冲突

--- App.vue
+++ App.vue
@@ -87,15 +87,14 @@
 </script>
 
 <style lang="scss">
+	@import '@/uni_modules/uni-scss/index.scss';
+	/* #ifndef APP-PLUS-NVUE */
+	/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
+	@import './common/uni.css';
+	@import '@/static/customicons.css';
 	@import "uview-ui/index.scss";
 	@import "common/demo.scss";
 
-	@import '@/uni_modules/uni-scss/index.scss';
-	// /* #ifndef APP-PLUS-NVUE */
-	// /* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
-	// @import './common/uni.css';
-	@import '@/static/customicons.css';
-	@import "@/uview-ui/index.scss";
 
 	/* H5 兼容 pc 所需 */
 	/* #ifdef H5 */
